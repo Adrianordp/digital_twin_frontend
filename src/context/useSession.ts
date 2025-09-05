e43409067032972e7f3df146ecbf581fd0c2ed51
Adriano Rodrigues <adrianordp1@gmail.com>
2025-09-04 23:09:52 -0400

    fix[src/context/useSession.ts]: Correct re-export of useSession from useSessionHook

--- src/context/useSession.ts
+++ src/context/useSession.ts
@@ -1,6 +1,6 @@
 import useSessionHook from './useSessionHook';
 
 // Re-export the hook from useSessionHook file
-export { default as useSession } from './SessionContext';
+export { default as useSession } from './useSessionHook';
 
 export default useSessionHook;
