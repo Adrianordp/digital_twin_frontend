d0ccdcb0a7ad49934da465e064f606e57bb5e882
Adriano Rodrigues <adrianordp1@gmail.com>
2025-09-04 22:53:05 -0400

    feat[src/context/useSession.ts]: Re-export useSession from SessionContext
    This change introduces a new file for useSession that re-exports the hook from SessionContext, ensuring better organization and access to session management functionalities.

---
+++ src/context/useSession.ts
@@ -0,0 +1,6 @@
+import useSessionHook from './useSessionHook';
+
+// Re-export the hook from useSessionHook file
+export { default as useSession } from './SessionContext';
+
+export default useSessionHook;
